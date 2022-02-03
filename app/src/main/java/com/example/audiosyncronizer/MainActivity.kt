91fe1864ade40f6623a9813de3773f3d58fac0ba
Wendinoda Washaya <wendinoda.washaya@entelect.co.za>
2022-02-03 11:58:37 +0200

    added button to layout, permission for recording,soundmeter class

--- app/src/main/java/com/example/audiosyncronizer/MainActivity.kt
+++ app/src/main/java/com/example/audiosyncronizer/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.audiosyncronizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startRecordingButton: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startRecordingButton.setOnClickListener {
+
+        }
     }
 }
